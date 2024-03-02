8d2048129b90e9d284861248c1fd874ad52c4991
connerbrown <darthconner2000@gmail.com>
2024-03-01 20:55:01 -0700

    Implement chat.js functions to send messages to the chat.

--- chat.js
+++ chat.js
@@ -9,3 +9,15 @@ function signOut(){
     localStorage.removeItem("userName")
     window.location.href = "login.html"
 }
+
+function comment(){
+    let ul = document.createElement("ul")
+    let p1 = document.createElement("p")
+    let p2 = document.createElement("p")
+    ul.appendChild(p1)
+    ul.appendChild(p2)
+
+    p1.appendChild(document.createTextNode(localStorage.getItem("userName")))
+    p2.appendChild(document.createTextNode(document.getElementById("comment").value))
+    document.querySelector('#Chatbox').appendChild(ul)
+}
