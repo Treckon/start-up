58c91d63f2918f025e82f0e7a9e6a6ed3e10d987
connerbrown <darthconner2000@gmail.com>
2024-03-01 18:54:43 -0700

    Implement Redirect function if not logged in

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("starting index.js")
+function loginRedirect(){
+    let username = localStorage.getItem("userName")
+    if (username === null){
+        window.location.href = "login.html"
+    }
+}
+
+loginRedirect()
+
+console.log("noobmaster69")
+let user = 'noobmaster69';
+setInterval(() => {
+    console.log("interval set")
+    chat = `${user} has commented in Mistborn Chat`;
+    let ul = document.getElementById("#Notice")
+    let li = document.createElement("li")
+    li.appendChild(document.createTextNode(chat))
+    document.querySelector('#Notice').appendChild(li)
+}, 10000);
+
+let list = document.querySelectorAll("ul#Notice");
+console.log(list)
+if(list.length > 10){
+    console.log("removing notice")
+    setInterval(() =>{
+        list.removeChild(list.firstElementChild);
+        console.log("notice removed")
+    }, 5000)
+}
+
+
