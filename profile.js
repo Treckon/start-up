42cbe27af9daef37fcc9b94414634bb3cdb8c7ed
connerbrown <darthconner2000@gmail.com>
2024-03-01 20:22:29 -0700

    Implement profile.js such that the table reflects changes made in editprofile.html

--- profile.js
+++ profile.js
@@ -9,3 +9,21 @@ function signOut(){
     localStorage.removeItem("userName")
     window.location.href = "login.html"
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let series = ["Mistborn", "Wax and Wayne", "The Stormlight Archive", "Tress of the Emerald Sea", "Warbreaker", "Elantris"]
+
+    for (i of series){
+        let tr = document.createElement("tr")
+        let th1 = document.createElement("th")
+        let th2 = document.createElement("th")
+        tr.appendChild(th1)
+        tr.appendChild(th2)
+
+        th1.appendChild(document.createTextNode(i))
+        th2.appendChild(document.createTextNode(localStorage.getItem(i)))
+        document.querySelector('#progress').appendChild(tr)
+    }
+
+  });
+
