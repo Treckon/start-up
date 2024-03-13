3242763f63538d972f217e70b6ed4b36b1f308f3
connerbrown <darthconner2000@gmail.com>
2024-03-12 18:40:07 -0600

    Set up third party quoting feature

--- login.js
+++ login.js
@@ -3,3 +3,25 @@ function login() {
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "index.html";
   }
+
+function displayQuote(data) {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      let section = document.getElementById("login");
+      console.log(section);
+      let quotep = document.createElement("p");
+      let authorp = document.createElement("p");
+
+      quotep.textContent = data.content;
+      quotep.className = "instruction";
+      authorp.textContent = data.author;
+      authorp.className = "instruction";
+      console.log(quotep);
+      console.log(authorp);
+      section.appendChild(quotep);
+      section.appendChild(authorp);
+    });
+}
+
+displayQuote();
