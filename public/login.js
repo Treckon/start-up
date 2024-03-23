434ba0998626c114e1b162d6f7bac5ce4269a2af
connerbrown <darthconner2000@gmail.com>
2024-03-22 21:06:56 -0600

    login changes and remove dbConfig.json

--- public/login.js
+++ public/login.js
@@ -1,8 +1,55 @@
-function login() {
-    const nameEl = document.querySelector("#email");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "index.html";
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+async function login() {
+  const nameEl = document.querySelector("#email");
+  const passwordEl = document.querySelector("#password");
+
+  const query = { userName: nameEl.value, password: passwordEl.value };
+  const options = {
+    limit: 1,
+  };
+
+  const cursor = collection.find(query, options);
+  const user = await cursor.toArray();
+  
+  if (length(user) < 1){
+    let section = document.getElementById("login");
+    let p = document.createElement("p");
+    let node = document.createTextNode("Username or Password Incorrect");
+    p.appendChild(node)
   }
+
+  localStorage.setItem("userName", nameEl.value);
+  window.location.href = "index.html";
+}
+
+async function signup() {
+  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}/bnb`;
+  const client = new MongoClient(url);
+  const db = client.db('users');
+  const collection = db.collection('house');
+
+  // Test that you can connect to the database
+  (async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
+
+  // Insert a document
+  const nameEl = document.querySelector("#email");
+  const passwordEl = document.querySelector("#password");
+  const user = {
+    userName: nameEl.value,
+    password: passwordEl.value,
+  };
+  await collection.insertOne(house);
+  localStorage.setItem("userName", nameEl.value);
+  window.location.href = "index.html";
+}
 
 function displayQuote(data) {
   fetch('https://api.quotable.io/random')
